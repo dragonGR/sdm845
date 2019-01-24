5516bb6e1315c8df211bf994851206a041ef2719
Park Ju Hyung <qkrwngud825@gmail.com>
2019-01-23 20:54:47 -0500

    dsi: hardcode our panel dts settings
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    [@0ctobot: Squash arter97/android_kernel_oneplus_sdm845@660df76, arter97/android_kernel_oneplus_sdm845@90e0c78,
    and arter97/android_kernel_oneplus_sdm845@6643211]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1906,15 +1906,11 @@ error:
 static int dsi_panel_parse_misc_features(struct dsi_panel *panel,
 				     struct device_node *of_node)
 {
-	panel->ulps_enabled =
-		of_property_read_bool(of_node, "qcom,ulps-enabled");
-
+	panel->ulps_enabled = true;
 	pr_info("%s: ulps feature %s\n", __func__,
 		(panel->ulps_enabled ? "enabled" : "disabled"));
 
-	panel->ulps_suspend_enabled =
-		of_property_read_bool(of_node, "qcom,suspend-ulps-enabled");
-
+	panel->ulps_suspend_enabled = true;
 	pr_info("%s: ulps during suspend feature %s", __func__,
 		(panel->ulps_suspend_enabled ? "enabled" : "disabled"));
 
@@ -1924,8 +1920,8 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel,
 	panel->sync_broadcast_en = of_property_read_bool(of_node,
 			"qcom,cmd-sync-wait-broadcast");
 
-	panel->lp11_init = of_property_read_bool(of_node,
-			"qcom,mdss-dsi-lp11-init");
+	panel->lp11_init = false;
+
 	return 0;
 }
 
